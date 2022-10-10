e9f5c673fe739e04043a1770d8d460a2df449edc
CarlosR759 <80783790+CarlosR759@users.noreply.github.com>
2022-10-09 21:33:33 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import logging
 
 from models.hive import MatrixHive
 from models.problems.himmelblaus import HimmelBlaus
+from models.problems.setcovering import SetCovering 
 
 
 def solve_himmelBlaus(colony_size=50, iterations=100):
-    func = HimmelBlaus(max_value=5, min_value=-5)
+    #func = HimmelBlaus(max_value=5, min_value=-5)
+    func = SetCovering()
     hive = MatrixHive(func, colony_size=colony_size, iterations=iterations,
                       max_trials=10)
     logging.debug('COMENZANDO BUSQUEDA...\n')
